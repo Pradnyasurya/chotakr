c90023678fb6233e25ec16427f9e1a50171ca238
Pradnyasurya Kurane <kuranepradnyesh@gmail.com>
2023-12-18 22:53:48 +0530

    shortkr initial commit

---
+++ src/main/java/live/shortkr/shortkr/ShortkrApplication.java
@@ -0,0 +1,13 @@
+package live.shortkr.shortkr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortkrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortkrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/live/shortkr/shortkr/ShortkrApplicationTests.java
@@ -0,0 +1,13 @@
+package live.shortkr.shortkr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortkrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
